91263643df5baf1d805ff9526cb8da6b0e04a94d
Johvahn <johvahnbro@gmail.com>
2022-10-27 08:03:28 -0500

    Edited lab files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 /* Add your JavaScript to this file */
+window.onload = function() {
+    
+    var btn = document.querySelector("button");
+    var msg = document.getElementsbyClassName("message");
+    
+    btn.addEventListener("click", function(event) {
+        event.preventDefault();
+        
+        var input = document.getElementbyId("email").value;
+        
+        if (input != "") {
+            msg[0].innerHTML = "Thank you! Your email address " + input + " has been added to our mailing list!";
+        } 
+        else {
+            msg[0].innerHTML = "Please enter a valid email address";
+        }
+    });
+}
